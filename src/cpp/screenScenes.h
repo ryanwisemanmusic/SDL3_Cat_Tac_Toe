afbe98d18ae60e863ccb54ac479135ea3a050f33
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2025-03-05 20:50:33 -0800

    For deletion
    Since I found this approach was less than idea, we will eventually get rid of all these files for the final project.

---
+++ src/cpp/screenScenes.h
@@ -0,0 +1,14 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+enum class SceneState
+{
+    MAIN_MENU,
+    GAME,
+    END_SCREEN
+};
+
+// Function declarations for scene handling
+void handleMainMenu(SDL_Renderer* renderer, SceneState& currentScene);
+void handleGame(SDL_Renderer* renderer, SceneState& currentScene);
+void handleEndScreen(SDL_Renderer* renderer, SceneState& currentScene);
